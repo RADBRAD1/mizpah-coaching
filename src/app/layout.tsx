48493ba661e7c0a7d20c8e27bb7e511532fc6a12
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2024-04-21 16:28:08 -0400

    v1.00002

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,26 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import "./globals.css";
+import {DM_Sans} from 'next/font/google'
+
+const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm-sans',
+})
+ 
+
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // brackets {} in classnames are used to specify class inheritance.
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className = {dm_sans.variable}>
+      <body className = "bg-red-500">{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
